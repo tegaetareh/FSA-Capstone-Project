8d46343b185f9e031b87b999ac51d9cdc416d46f
Oghenetega Etareh <tega.etare@gmail.com>
2023-09-11 01:20:08 -0400

    Feature/Style search and filter

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -46,17 +46,23 @@ export default function Products() {
     return (
 
         < div className="allProducts">
-            <form onSubmit={handleSearch}>
-                <input className="searchbar" onChange={handleSearch} type="text" id="search" />
-            </form >
-            <form >
-                <input value="electronics" type="radio" name="category" id="eletronics" onChange={handleCategory} /><label>Eletronics</label>
-                <input value="jewelery" type="radio" name="category" id="jewelery" onChange={handleCategory} /><label>Jewelery</label>
-                <input value="men's clothing" type="radio" name="category" id="men's clothing" onChange={handleCategory} /><label>Mens Clothing</label>
-                <input value="women's clothing" type="radio" name="category" id="women's clothing" onChange={handleCategory} /><label>Womens Clothing</label>
-                <input value="all" type="radio" name="category" id="all" onChange={handleCategory} /><label>All</label>
 
-            </form>
+            <div className="ssf">
+                {/* ssf is search sort and filter */}
+
+                <form onSubmit={handleSearch}>
+                <b>Search: </b><input className="searchbar" onChange={handleSearch} type="text" id="search" />
+                </form >
+                <form ><b>Filter: </b>
+                    <input value="electronics" type="radio" name="category" id="eletronics" onChange={handleCategory} /><label>Eletronics</label>
+                    <input value="jewelery" type="radio" name="category" id="jewelery" onChange={handleCategory} /><label>Jewelery</label>
+                    <input value="men's clothing" type="radio" name="category" id="men's clothing" onChange={handleCategory} /><label>Mens Clothing</label>
+                    <input value="women's clothing" type="radio" name="category" id="women's clothing" onChange={handleCategory} /><label>Womens Clothing</label>
+                    <input value="all" type="radio" name="category" id="all" onChange={handleCategory} /><label>All</label>
+
+                </form>
+            </div>
+
             <main >
                 {filteredByCategory ?
                     // TODO: SHOW LOADING BAR WHILE PRODUCT IS LOADING
